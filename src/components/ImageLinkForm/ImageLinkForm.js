ea9b6da2ae4f1ac755d4e9713e8edba95707c666
CJ Roberts <cjroberts2013@gmail.com>
2020-10-08 09:44:54 -0400

    Created components to show the sign out nav button, the logo, and the state of the image link form. Also styled the background

---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImageLinkForm = () => {
+	return (
+		<div className="">
+			<p className="f3">
+				{"This app will detect faces in your pictures. Give it a try!"}
+			</p>
+		</div>
+	);
+};
+
+export default ImageLinkForm;
